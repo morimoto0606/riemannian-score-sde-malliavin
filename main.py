e804c68506d08b6ff481d79d17e6e6b899975fbf
morimoto0606 <morimoto0606@yahoo.co.jp>
2026-09-09 11:50:07 +0900

    modified bug in main.py

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ def main(cfg):
     os.environ["GEOMSTATS_BACKEND"] = "jax"
     os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
     os.environ["WANDB_START_METHOD"] = "thread"
-    if (cfg.get("enable_x64", False)
-            and (cfg.get("manifold", {}).get("_target_") == "riemannian_score_sde.spd.AffineSPD"
-                 or getattr(cfg.get("teacher"), "_target_", None)
-                 == "riemannian_score_sde.teachers.SpectrumTeacher")):
+    is_affine_spd = (
+        cfg.get("manifold", {}).get("_target_")
+        == "riemannian_score_sde.spd.AffineSPD"
+    )
+    is_spectrum_teacher = (
+        getattr(cfg.get("teacher"), "_target_", None)
+        == "riemannian_score_sde.teachers.SpectrumTeacher"
+    )
+    if cfg.get("enable_x64", False) and (is_affine_spd or is_spectrum_teacher):
         os.environ["JAX_ENABLE_X64"] = "True"
 
     from run import run
