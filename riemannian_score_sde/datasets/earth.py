ecb28475141ff657c1d9cb652b218c88d7b4161f
morimoto0606 <morimoto0606@yahoo.co.jp>
2026-09-11 00:53:18 +0900

    modified postprocess

--- riemannian_score_sde/datasets/earth.py
+++ riemannian_score_sde/datasets/earth.py
@@ -5,6 +5,7 @@ from score_sde.datasets import CSVDataset
 
 import geomstats as gs
 import jax.numpy as jnp
+from riemannian_score_sde.earth_data import EARTH_DATA, spherical_angles
 
 
 class SphericalDataset(CSVDataset):
@@ -12,15 +13,14 @@ class SphericalDataset(CSVDataset):
         super().__init__(file, delimiter=delimiter, skip_header=skip_header)
 
         self.manifold = gs.geometry.hypersphere.Hypersphere(2)
-        self.intrinsic_data = (
-            jnp.pi * (self.data / 180.0) + jnp.array([jnp.pi / 2, jnp.pi])[None, :]
-        )
+        self.intrinsic_data = spherical_angles(self.data, jnp)
         self.data = self.manifold.spherical_to_extrinsic(self.intrinsic_data)
 
 
 class VolcanicErruption(SphericalDataset):
     def __init__(self, data_dir="data", **kwargs):
-        super().__init__(os.path.join(data_dir, "volerup.csv"), skip_header=2)
+        spec = EARTH_DATA["volcanoe"]
+        super().__init__(os.path.join(data_dir, spec["file"]), skip_header=spec["skip_header"])
 
 
 class Fire(SphericalDataset):
@@ -30,9 +30,11 @@ class Fire(SphericalDataset):
 
 class Flood(SphericalDataset):
     def __init__(self, data_dir="data", **kwargs):
-        super().__init__(os.path.join(data_dir, "flood.csv"), skip_header=2)
+        spec = EARTH_DATA["flood"]
+        super().__init__(os.path.join(data_dir, spec["file"]), skip_header=spec["skip_header"])
 
 
 class Earthquake(SphericalDataset):
     def __init__(self, data_dir="data", **kwargs):
-        super().__init__(os.path.join(data_dir, "quakes_all.csv"), skip_header=4)
+        spec = EARTH_DATA["earthquake"]
+        super().__init__(os.path.join(data_dir, spec["file"]), skip_header=spec["skip_header"])
